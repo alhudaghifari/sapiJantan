92758dadb5bd9e5670600cc01662ea4964fb9c46
afghifari <alhudaghifari@gmail.com>
2016-09-27 17:20:10 +0700

    id jadi String, before : shor. and ada SAPI :) :*

--- aiutils/src/aiutils/StudyRoom.java
+++ aiutils/src/aiutils/StudyRoom.java
@@ -23,7 +23,7 @@
                           |     |
 * jelek ya kelinci nya. Peang pletat pletot gitu.
 */
-package schedulling;
+package aiutils;
 
 /**
  * Kelas untuk kelas tempat kuliah.
@@ -60,3 +60,25 @@ public class StudyRoom {
     }
     
 }
+
+
+/**
+ * sssssssssssssss       aaaaaaaaaaaaaaaaaaaaaaaa       ppppppppppppppppppppppp       iiiiiiiiii
+ * sssssssssssssss       aaaaaaaa        aaaaaaaa       pppppp          ppppppp       iiiiiiiiii
+ * sssssssssssssss       aaaaaaa          aaaaaaa       pppppp         pppppppp       iiiiiiiiii
+ * ssssss                aaaaaa             aaaaa       pppppp         pppppppp       iiiiiiiiii 
+ * ssssss                aaaaaa            aaaaaa       pppppp        ppppppppp       iiiiiiiiii
+ * ssssss                aaaaaa            aaaaaa       pppppp       pppppppppp       iiiiiiiiii
+ * sssssssssssssss       aaaaaa            aaaaaa       pppppp    ppppppppppppp       iiiiiiiiii
+ * sssssssssssssss       aaaaaa            aaaaaa       pppppp   pppppppppppppp       iiiiiiiiii
+ * sssssssssssssss       aaaaaaaaaaaaaaaaaaaaaaaa       ppppppppppppppppppppppp       iiiiiiiiii
+ *         sssssss       aaaaaaaaaaaaaaaaaaaaaaaa       pppppp                        iiiiiiiiii
+ *         sssssss       aaaaaaaaaaaaaaaaaaaaaaaa       pppppp                        iiiiiiiiii
+ *         sssssss       aaaaaaaaaaaaaaaaaaaaaaaa       pppppp                        iiiiiiiiii
+ * sssssssssssssss       aaaaaa            aaaaaa       pppppp                        iiiiiiiiii
+ * sssssssssssssss       aaaaaa            aaaaaa       pppppp                        iiiiiiiiii
+ * sssssssssssssss       aaaaaa            aaaaaa       pppppp                        iiiiiiiiii
+ * 
+ * 
+ * 
+ ****/
